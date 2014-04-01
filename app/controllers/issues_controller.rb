44684809cce06f444f67b7e5ea2b35bb099ddcea
pat-whitrock <patrick.whitrock@gmail.com>
2014-04-01 14:02:25 -0400

    form can post issues directly to github

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,24 +1,38 @@
 class IssuesController < ApplicationController
 
-	def index
-	end
+	# def index
+	# end
 
 	def new
+		@issue = Issue.new
 	end
 
 	def create
+		create_github_issue(issue_params)
+		redirect_to issues_new_path
 	end
 
-	def show
+	# def show
+	# end
+
+	# def update
+	# end
+
+	# def edit
+	# end
+
+	# def destroy
+	# end
+
+	private
+	def issue_params
+		params.require(:issue).permit(:email, :title, :body, :repo)
 	end
 
-	def update
-	end
-
-	def edit
-	end
-
-	def destroy
+	def create_github_issue(attributes)
+		client = Octokit::Client.new(:access_token => ENV["GITHUB_ACCESS_TOKEN"])
+		user = client.user.login
+		client.create_issue('pat-whitrock/gitshoes', attributes['title'], attributes['body'])
 	end
 
 end
