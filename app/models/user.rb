23d3cedecece04012915737ee49c33782e68c0b8
wontaeyang <wontaeyang@gmail.com>
2014-04-01 18:05:50 -0400

    added devise

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+    devise :omniauthable, :omniauth_providers => [:github]
+   		#:database_authenticatable, :registerable,
+        #:recoverable, :rememberable, :trackable, :validatable, 
+
+  def self.find_for_github_oauth(auth)
+  	where(auth.slice(:provider, :token)).first_or_create do |user|
+  		user.provider = auth.provider
+  		user.token = auth.credentials.token
+  	end
+  end
+end
