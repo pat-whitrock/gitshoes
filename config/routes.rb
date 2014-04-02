753acbd24042f9d47b7768221bb29818002f5f48
wontaeyang <wontaeyang@gmail.com>
2014-04-02 18:14:34 -0400

    fixed sign out

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => {:omniauth_callbacks => 'users/omniauth_callbacks'}
   devise_scope :user do 
-    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_sessions
+    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
   end
   root 'static#index'
 
