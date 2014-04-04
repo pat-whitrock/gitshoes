2138d1c23dfc5148b46f0d7e6c975e66edbcd63f
pat-whitrock <patrick.whitrock@gmail.com>
2014-04-04 14:11:33 -0400

    remove environment variable for access token with repository owner token

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -2,7 +2,7 @@ class Issue < ActiveRecord::Base
 	belongs_to :repo
 
 	def self.create_github_issue(attributes, repository)
-		client = Octokit::Client.new(:access_token => ENV["GITHUB_ACCESS_TOKEN"])
+		client = Octokit::Client.new(:access_token => repository.token)
 		user = client.user.login
 		client.create_issue(repository.full_name, attributes['title'], attributes['body'])
 	end
