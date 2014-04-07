089021f5e567abcd3ddc7c037ec3c143ba3de814
pat-whitrock <patrick.whitrock@gmail.com>
2014-04-07 13:10:39 -0400

    prevent caching limited to repos#new

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -1,5 +1,12 @@
 class ReposController < ApplicationController
 	protect_from_forgery :except => :show
+	before_action :set_cache_buster, :only => [:new]
+
+  def set_cache_buster
+    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Fri, 01 Jan 1990 00:00:00 GMT"
+  end
 
 	def index
 		@repos = current_user.repos
