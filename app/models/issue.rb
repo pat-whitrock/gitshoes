ee79feb81c4237559296cb632d52c8b7d6c82e03
pat-whitrock <patrick.whitrock@gmail.com>
2014-04-17 15:30:33 -0400

    save screenshots in tempfiles

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,3 +1,4 @@
+require 'tempfile'
 class Issue < ActiveRecord::Base
 	belongs_to :repo
 	# accepts_nested_attributes_for :widget
@@ -5,10 +6,10 @@ class Issue < ActiveRecord::Base
 
 	def self.create_github_issue(attributes, repository)
 		uri = URI::Data.new(attributes['data_image'])
-		# encoded_uri = Base64.encode64(open(uri) { |io| io.read })
-		f = File.new('newtest.png', 'w+')
+		f = Tempfile.new(['screenshot', '.png'])
 		f.write(uri.data.force_encoding('UTF-8'))
 		f.close
+		image_file_name = f.path.split('/').last
 
 		connection = Fog::Storage.new({
 			:provider           => 'AWS',
@@ -18,17 +19,17 @@ class Issue < ActiveRecord::Base
 		})
 		directory = connection.directories.first
 		file = directory.files.create(
-			:key    => 'newtest.png',
-			:body   => File.open("newtest.png"),
+			:key    => image_file_name,
+			:body   => IO.read(f.path),
 			:public => true
 		)
-		image_url = "https://s3-us-west-2.amazonaws.com/gitshoes/newtest.png"
+		image_url = "https://s3-us-west-2.amazonaws.com/gitshoes/#{image_file_name}"
 		client = Octokit::Client.new(:access_token => repository.users[0].token)
 		user = client.user.login
 		if attributes['email'].length > 0
 			issue_body = "From: #{attributes['email']}\n\n#{attributes['body']}\n\nScreenshot: #{image_url}"
 		else
-			issue_body = attributes['body']
+			issue_body = "#{attributes['body']}\n\nScreenshot: #{image_url}"
 		end
 		client.create_issue(repository.full_name, attributes['title'], issue_body)
 	end
