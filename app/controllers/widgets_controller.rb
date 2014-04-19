454f82096ab17a781489b18b44342c40cf916213
pat-whitrock <patrick.whitrock@gmail.com>
2014-04-18 21:33:27 -0400

    display cloned feedback widget in div

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -1,8 +1,13 @@
 class WidgetsController < ApplicationController
+	protect_from_forgery :except => [:edit]
 
 	def edit
 		@repo = Repo.find(params[:id])
 		@widget = @repo.widget
+		respond_to do |format|
+			format.html
+			format.js
+    end
 	end
 
 	def update
--- app/helpers/widgets_helper.rb
+++ app/helpers/widgets_helper.rb
@@ -1,2 +1,2 @@
-module WidgetHelper
+module WidgetsHelper
 end
