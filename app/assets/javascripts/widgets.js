21de62d12a1232e599cde04f4c3d82bf0a65bcfa
pat-whitrock <patrick.whitrock@gmail.com>
2014-04-20 23:03:26 -0400

    live edit of color complete for zoom

--- app/assets/javascripts/widgets.js
+++ app/assets/javascripts/widgets.js
@@ -69,5 +69,42 @@ $(function() {
 		removeActive();
 		switchToText();
 	});
+});
 
-})
+function updateWidgetTabTextColor() {
+	$('#widget_tab_font_color').on('change', function(e) {
+		$('#feedback-clone-zoom-gitshoes-button').css('color', $(this).val());
+	});
+}
+
+function updateWidgetTabBackgroundColor() {
+	$('#widget_tab_background_color').on('change', function(e) {
+		$('#feedback-clone-zoom-gitshoes-button').css('background-color', $(this).val());
+	});
+}
+
+function updateWidgetBodyFontColor() {
+	$('#widget_body_font_color').on('change', function(e) {
+		$('#clone-zoom-new-issue label').css('color', $(this).val());
+	});
+}
+
+function updateWidgetFormBackgroundColor() {
+	$('#widget_form_background_color').on('change', function(e) {
+		$('#gitshoes-clone-zoom-form').css('background-color', $(this).val());
+	});
+}
+
+function updateWidgetFormBorderColor() {
+	$('#widget_form_border_color').on('change', function(e) {
+		$('#gitshoes-clone-zoom-form').css('border-color', $(this).val());
+	});
+}
+
+$(function() {
+	updateWidgetTabTextColor();
+	updateWidgetTabBackgroundColor();
+	updateWidgetBodyFontColor();
+	updateWidgetFormBackgroundColor();
+	updateWidgetFormBorderColor();
+});
