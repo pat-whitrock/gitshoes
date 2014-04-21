aaa3c7f86c591c803305c82e3558c15752994570
pat-whitrock <patrick.whitrock@gmail.com>
2014-04-21 11:35:34 -0400

    live update color affects both views

--- app/assets/javascripts/widgets.js
+++ app/assets/javascripts/widgets.js
@@ -80,30 +80,35 @@ $(function() {
 function updateWidgetTabTextColor() {
 	$('#widget_tab_font_color').on('change', function(e) {
 		$('#feedback-clone-zoom-gitshoes-button').css('color', $(this).val());
+		$('#feedback-clone-gitshoes-button').css('color', $(this).val());
 	});
 }
 
 function updateWidgetTabBackgroundColor() {
 	$('#widget_tab_background_color').on('change', function(e) {
 		$('#feedback-clone-zoom-gitshoes-button').css('background-color', $(this).val());
+		$('#feedback-clone-gitshoes-button').css('background-color', $(this).val());
 	});
 }
 
 function updateWidgetBodyFontColor() {
 	$('#widget_body_font_color').on('change', function(e) {
 		$('#clone-zoom-new-issue label').css('color', $(this).val());
+		$('#clone-new-issue label').css('color', $(this).val());
 	});
 }
 
 function updateWidgetFormBackgroundColor() {
 	$('#widget_form_background_color').on('change', function(e) {
 		$('#gitshoes-clone-zoom-form').css('background-color', $(this).val());
+		$('#gitshoes-clone-form').css('background-color', $(this).val());
 	});
 }
 
 function updateWidgetFormBorderColor() {
 	$('#widget_form_border_color').on('change', function(e) {
 		$('#gitshoes-clone-zoom-form').css('border-color', $(this).val());
+		$('#gitshoes-clone-form').css('border-color', $(this).val());
 	});
 }
 
@@ -180,7 +185,7 @@ $(function() {
 	updateWidgetButtonFontSize();
 });
 
-// Leaving space for Orientation
+// Functions for live update Orientation
 
 function updateWidgetEdgeDistance() {
 	$('#widget_edge').on('change', function(e) {
@@ -205,6 +210,8 @@ function updateWidgetOffset() {
 		$('#feedback-clone').css($('#widget_orientation').val(), $(this).val());
 	});
 }
+
+// Call functions for live update Orientation
 
 $(function() {
 	updateWidgetEdgeDistance();
