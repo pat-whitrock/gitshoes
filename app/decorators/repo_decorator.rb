f81924dac8f955943054e235c8438af5af7b92f4
Pat Whitrock <pwhitrock@constantcontact.com>
2015-12-14 20:33:35 -0500

    More cleanup for ReposController#new

--- app/decorators/repo_decorator.rb
+++ app/decorators/repo_decorator.rb
@@ -3,7 +3,19 @@ class RepoDecorator < SimpleDelegator
     "<p><strong>Description:</strong> #{super}</p>" if super.present?
   end
 
+  def gh_updated_at
+    github_updated_at.strftime '%b %d, %Y'
+  end
+
   def language
     "<small>// #{super}</small>" if super.present?
+  end
+
+  def organization
+    full_name.split('/').first
+  end
+
+  def title
+    name.titleize
   end
 end
--- app/models/repo_collection.rb
+++ app/models/repo_collection.rb
@@ -1,10 +1,15 @@
 class RepoCollection
+  include ActionView::Helpers::TextHelper
   include Enumerable
 
   def initialize source: source
     @repos = source.all.map do |repo|
       RepoDecorator.new repo
     end
+  end
+
+  def available
+    "#{pluralize(repos.count, 'repository')} available"
   end
 
   def each &block
